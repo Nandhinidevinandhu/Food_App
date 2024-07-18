763d1ae0a25eaee85821201c0badbf34c255f2d2
Nandhinidevinandhu <nandhinidevi096@gmail.com>
2024-07-18 23:10:03 +0530

    Meals summary added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
-import React, { Fragment } from "react";
-import Header from "./components/Layout/Header";
+import { Fragment } from 'react';
+
+import Header from './components/Layout/Header';
+import Meals from './components/Meals/Meals';
 
 function App() {
   return (
-    <React.Fragment>
+    <Fragment>
       <Header />
-    </React.Fragment>
+      <main>
+        <Meals />
+      </main>
+    </Fragment>
   );
 }
 
---
+++ src/components/Meals/Meals.js
@@ -0,0 +1,14 @@
+import { Fragment } from 'react';
+
+import MealsSummary from './MealsSummary';
+
+
+const Meals = () => {
+  return (
+    <Fragment>
+      <MealsSummary />   
+    </Fragment>
+  );
+};
+
+export default Meals;
